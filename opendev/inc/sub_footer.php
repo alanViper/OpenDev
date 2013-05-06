a6d989127918cac536e7682a050a907ec20208a5
Alan <alan@xr1.com.br>
2013-05-06 11:18:38 -0300

    Atualização de arquivos e remoção de erros.

---
+++ opendev/inc/sub_footer.php
@@ -0,0 +1,43 @@
+	<section>
+		<div id="box_blog" class="box">
+			<h1>Blog</h1>
+			<ul class="box_list">
+			<?php query_posts ('posts_per_page = 6'); ?>
+				<?php if (have_posts()): while (have_posts()) : the_post();?>
+				<li>
+					<a href="<?php the_Permalink()?>"><?php the_time('j/m/Y');?> - <?php the_title();?></a>
+				</li>
+				<?php endwhile; else:?>
+				<li>
+					<a href="">Ainda não temos nenhum post :(</a>
+				</li>
+				<?php endif;?> 
+			</ul>
+		</div>
+	</section>
+
+	<section>
+		<div id="box_flickr" class="box">
+			<h1>Fotos</h1>
+			<a href="http://www.flickr.com/photos/95051897@N03/"><img src="<?php bloginfo('template_directory'); ?>/images/flickr.png" id="flickr" alt="" title="" /></a>
+			<ul id="box_photos">
+				<script type="text/javascript">
+				function jsonFlickrFeed(url){
+					n = 0;
+					while(url.items[n]){
+						if(n < 15){
+							_m = url.items[n].media.m;
+							_s = _m.replace("_m", "_s");
+							img = "<li><a href=\""+url.items[n].link+"\" target=\"_blank\">";
+							img += "<img src=\""+_s+"\" alt=\""+url.items[n].title+"\" width=\"65\" height=\"65\" />";
+							img += "</a></li>";
+							document.write(img);
+						}
+						n++;
+					}
+				}
+				</script>
+				<script src="http://api.flickr.com/services/feeds/photoset.gne?set=72157633274499299&nsid=95051897@N03&lang=pt-br&format=json" type="text/javascript"></script>
+			</ul>
+		</div>
+	</section>
---
+++ opendev/page-home.php
@@ -0,0 +1,38 @@
+<?php get_header(); ?>
+<div class="wrapper">
+	<section>
+		<div class="content">
+			<h1>O projeto</h1>
+
+			<p>O OpenDev é um grupo de tecnologia, em Nova Friburgo, com fins técnicos. Um lugar onde desenvolvedores encontram empreendedores e empreendendores encontram desenvolvedores.</p>
+
+			<p>Propostas do grupo:</p>
+			<ul>
+				<li>Discutir e disseminar o conhecimento tecnológico;</li>
+				<li>Conhecer pessoas da área;</li>
+				<li>Difundir ideias;</li>
+			</ul>
+
+			<p>Quando o assunto é de teor mais técnico as pessoas tendem a se afastar, vemos muitos eventos e grupos de TI, porém com foco maior em gestão. Deixando de lado os conhecimentos específicos, que são pré-requisitos para bons projetos.</p>
+
+			<p>Não iremos abolir temas como gestão, empreendedorismo, processos de fabricação de cervejas [;D] e outros; mas iremos sempre trazer para o foco que são os aspectos técnicos para o desenvolvimento.</p>
+
+			<p>O grupo é aberto a todos da área, seja você eng. de software, eng. elétrico, gestor, investidor ou designer.</p>
+			 
+			<h2>Próxima reunião:</h2>
+			<p>Dia 16/05 às 18h30min.</p>
+
+			<h2>Onde nos achar...</h2>
+			<p>
+				Av. Euterpe Friburguense, 21<br />
+				Centro - Nova Friburgo - RJ
+				<br /><strong>Local:</strong> Casarão de Minas Chopperia
+			</p>
+			
+			<iframe width="800" height="350" frameborder="0" style="border:1px solid #000;" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com.br/maps?f=q&amp;source=s_q&amp;hl=pt-BR&amp;geocode=&amp;q=Av.+Euterpe+Friburguense,+21&amp;aq=&amp;sll=-22.27697,-42.534149&amp;sspn=0.002725,0.005284&amp;ie=UTF8&amp;hq=&amp;hnear=Av.+Euterpe+Friburguense,+21+-+Nova+Friburgo+-+Rio+de+Janeiro,+28605-130&amp;ll=-22.276719,-42.533713&amp;spn=0.010822,0.021136&amp;t=m&amp;z=14&amp;output=embed"></iframe><br /><small><a href="https://maps.google.com.br/maps?f=q&amp;source=embed&amp;hl=pt-BR&amp;geocode=&amp;q=Av.+Euterpe+Friburguense,+21&amp;aq=&amp;sll=-22.27697,-42.534149&amp;sspn=0.002725,0.005284&amp;ie=UTF8&amp;hq=&amp;hnear=Av.+Euterpe+Friburguense,+21+-+Nova+Friburgo+-+Rio+de+Janeiro,+28605-130&amp;ll=-22.276719,-42.533713&amp;spn=0.010822,0.021136&amp;t=m&amp;z=14" style="color:#0000FF;text-align:left">Exibir mapa ampliado</a></small>
+		</div>
+	</section>
+
+	<?php require_once(TEMPLATEPATH.'/inc/sub_footer.php'); ?>
+</div>
+<?php get_footer(); ?>
