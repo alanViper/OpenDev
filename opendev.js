c6148004bf8e500df7e092eb7f21dadded90bf93
Alan <alan@xr1.com.br>
2013-05-06 11:16:50 -0300

    Correções.

--- opendev.js
+++
@@ -1,8 +0,0 @@
-window.onscroll = function(){
-	go_top = document.getElementById("go_top");
-	scrollY = window.pageYOffset;
-	if(scrollY > 142)
-		go_top.style.display = "block";
-	else
-		go_top.style.display = "none";
-}
