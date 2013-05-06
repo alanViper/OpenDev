a1424b36d550b95217c2ae093a753b5555a78206
Alan <alan@xr1.com.br>
2013-05-06 11:12:25 -0300

    Atualização do branch.

---
+++ README.md
@@ -0,0 +1,9 @@
+Template #OpenDev
+====================
+
+Template desenvolvido para o site do **#OpenDev**.
+
+**Livre** para uso e modificações.
+
+**html** > Contém os arquivos html+css+js.
+**opendev** > Template montado para o WordPress.
---
+++ opendev/page-contato.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+<div class="wrapper">
+	<section>
+		<div class="content">
+			<h1>Contato</h1>
+
+			<p>Para entrar em contato conosco, basta enviar um e-mail para: contato@opendev.com.br<br />Ou participar do <a href="#">grupo no facebook</a>.</p>
+		</div>
+	</section>
+	<?php require_once(TEMPLATEPATH.'/inc/sub_footer.php'); ?>
+</div>
+<?php get_footer(); ?>
